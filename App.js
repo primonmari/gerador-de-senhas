5ca782bb2639369f3806a5b7821ad9626d0b0c08
primonmari <114789403+primonmari@users.noreply.github.com>
2024-02-29 22:33:14 -0300

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start pp!</Text>
+      <Text>Open up App.js to stap!</Text>
       <StatusBar style="auto" />
     </View>
   );
